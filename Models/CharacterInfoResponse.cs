cf0ba8c78a6d58fef57ee6a6e927166a111c7064
Riaan Terblanche <riaan@firsttech.digital>
2021-09-22 20:19:05 +0200

    Add project files.

---
+++ Models/CharacterInfoResponse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marvel.Models
+{
+    public class CharacterInfoResponse
+    {
+        public string AttributionText { get; set; }
+        public Datawrapper Data { get; set; }
+
+        public class Datawrapper
+        {
+            public List<Result> Results { get; set; }
+        }
+
+        public class Result
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public Image Thumbnail { get; set; }
+            public ResourceList Comics { get; set; }
+
+            public class Image
+            {
+                public string Path { get; set; }
+                public string Extension { get; set; }
+            }
+
+            public class ResourceList
+            {
+                public int Available { get; set; }
+                public int Returned { get; set; }
+                public string CollectionURI { get; set; }
+                public List<Items> Items { get; set; }
+            }
+            public class Items
+            {
+                public string ResourceURI { get; set; }
+                public string Name { get; set; }
+            }
+
+        }
+    }
+}
